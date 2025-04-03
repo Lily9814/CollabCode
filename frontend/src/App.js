fae3425a5ca4a5d84cad05667fb9014d4c0efdd4
Lorrie Savage <lorriesavage@10-18-50-10.dynapool.wireless.nyu.edu>
2025-04-03 17:45:14 -0400

    testing + cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          "Hi"
+          "Hello this is a test"
         </p>
         <a
           className="App-link"
